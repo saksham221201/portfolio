8f1bdd2d714601f990e109a2c302cb6ff5d073c7
Saksham <sakshamjoshi1975@gmail.com>
2026-07-15 22:15:29 +0530

    Rectified the Resume link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -48,7 +48,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1kLPkZeezVxD0me29RVDmT73P8EeNAWgJ/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1kLPkZeezVxD0me29RVDmT73P8EeNAWgJ/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
@@ -73,7 +73,7 @@ const About = () => {
           >
             <img
               src={profileImage}
-              alt="Tarun Kaushik"
+              alt="Saksham Joshi"
               className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
             />
           </Tilt>
